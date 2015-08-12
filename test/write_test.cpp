c74ee0c95f38012a0b452c709c512fb0cac735fa
ljfa-ag <ljfa-ag@web.de>
2015-08-12 13:34:09 +0200

    Add basic tests for stream_writer

---
+++ test/write_test.cpp
@@ -0,0 +1,61 @@
+/*
+ * libnbt++ - A library for the Minecraft Named Binary Tag format.
+ * Copyright (C) 2013, 2015  ljfa-ag
+ *
+ * This file is part of libnbt++.
+ *
+ * libnbt++ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libnbt++ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libnbt++.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "microtest.h"
+#include "io/stream_writer.h"
+#include <iostream>
+#include <sstream>
+
+using namespace nbt;
+
+void test_stream_writer_big()
+{
+    std::ostringstream os;
+    nbt::io::stream_writer writer(os);
+
+    ASSERT(&writer.get_ostr() == &os);
+    ASSERT(writer.get_endian() == endian::big);
+
+    writer.write_type(tag_type::End);
+    writer.write_type(tag_type::Long);
+    writer.write_type(tag_type::Int_Array);
+
+    writer.write_num(int64_t(0x0102030405060708));
+
+    writer.write_string("foobar");
+
+    ASSERT(os);
+    std::string expected{
+        0, //tag_type::End
+        4, //tag_type::Long
+        11, //tag_type::Int_Array
+
+        0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, //0x0102030405060708 in Big Endian
+
+        0x00, 0x06, //string length in Big Endian
+        'f', 'o', 'o', 'b', 'a', 'r'
+    };
+    ASSERT(os.str() == expected);
+    std::clog << "test_stream_writer_big passed" << std::endl;
+}
+
+int main()
+{
+    test_stream_writer_big();
+}
